aabf3903138b9b505267f3a94e92e3be4172195b
kashish <kashish.shah@searce.com>
2022-07-14 19:53:00 +0530

    Added environment url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: 'http://localhost:3000/',
+  url: 'https://new-poc-microsite.el.r.appspot.com/',
   header1:'Content-type',
   header2:'application/json'
 };
